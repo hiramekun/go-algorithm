ef99d03f54779586edf2921bf16d7d5bfd43c10f
hiramekun <thescript1210@gmail.com>
2024-10-23 02:18:13 +0900

    update

--- leetcode/removeelement.go
+++ leetcode/removeelement.go
@@ -1,17 +1,15 @@
 package leetcode
 
-import "slices"
-
 // https://leetcode.com/problems/remove-element?envType=study-plan-v2&envId=top-interview-150
 func removeElement(nums []int, val int) int {
-	ret := 0
-	for i, e := range nums {
-		if e != val {
-			ret += 1
-		} else {
-			nums[i] = 51
+	j := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[j], nums[i] = nums[i], nums[j]
+			j++
 		}
 	}
-	slices.Sort(nums)
-	return ret
+
+	return j
 }
