48c0b5585facd47d3ca99ca15379c39c51febbac
hiramekun <thescript1210@gmail.com>
2024-10-26 00:48:26 +0900

    update

--- leetcode/removeduplicatesfromsortedarray.go
+++ leetcode/removeduplicatesfromsortedarray.go
@@ -2,17 +2,13 @@ package leetcode
 
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array?envType=study-plan-v2&envId=top-interview-150
 func removeDuplicates(nums []int) int {
-	if len(nums) < 2 {
-		return len(nums)
-	}
-	i := 1
-	for k := 1; k < len(nums); k++ {
-		if nums[k] != nums[k-1] {
-			if i != k {
-				nums[i] = nums[k]
-			}
-			i++
+	left := 0
+
+	for right := 0; right < len(nums); right++ {
+		if nums[left] != nums[right] {
+			nums[left+1] = nums[right]
+			left++
 		}
 	}
-	return i
+	return len(nums[:left+1])
 }
